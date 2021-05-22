949dd97ccc9a534008eec3b967b18f5e9f976125
z3DD3r <2339201+z3DD3r@users.noreply.github.com>
2021-05-22 23:33:20 +0300

    Cedric: Tweaks for 'low ram' devices
     * Most of these tweaks were taken from devices with GO configuration
     * We can use these tweaks without turning our device into GO version
     * Our device can be treated as 'low ram' beaucase of only 2Gb of RAM

---
+++ lowram/device.mk
@@ -0,0 +1,48 @@
+# Speed profile services and wifi-service to reduce RAM and storage.
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+
+# Always preopt extracted APKs to prevent extracting out of the APK for gms
+# modules.
+PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
+
+# Use a profile based boot image for this device. Note that this is currently a
+# generic profile and not Android Go optimized.
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+
+# Do not generate libartd.
+PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
+
+# Strip the local variable table and the local variable type table to reduce
+# the size of the system image. This has no bearing on stack traces, but will
+# leave less information available via JDWP.
+PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
+
+# Disable Scudo outside of eng builds to save RAM.
+PRODUCT_DISABLE_SCUDO := true
+
+# Do not spin up a separate process for the network stack on go devices, use an in-process APK.
+PRODUCT_PACKAGES += InProcessNetworkStack
+
+# madvise random in ART to reduce page cache thrashing.
+dalvik.vm.madvise-random=true
+
+# Disable unnecessary debugging
+WITH_DEXPREOPT_DEBUG_INFO := false
+USE_DEX2OAT_DEBUG := false
+
+# set threshold to filter unused apps
+pm.dexopt.downgrade_after_inactive_days=10
+
+# set the compiler filter for shared apks to quicken.
+# Rationale: speed has a lot of dex code expansion, it uses more ram and space
+# compared to quicken. Using quicken for shared APKs on Go devices may save RAM.
+# Note that this is a trade-off: here we trade clean pages for dirty pages,
+# extra cpu and battery. That's because the quicken files will be jit-ed in all
+# the processes that load of shared apk and the code cache is not shared.
+# Some notable apps that will be affected by this are gms and chrome.
+# b/65591595.
+# pm.dexopt.shared=quicken
+
+# The target has no boot jars to check
+SKIP_BOOT_JARS_CHECK := true
---
+++ product/qcom-art.mk
@@ -0,0 +1,9 @@
+# Tweaks for 'low ram' devices
+$(call inherit-product-if-exists, $(LOCAL_PATH)/lowram/device.mk)
+
+# ART properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.dex2oat-swap=false \
+    ro.config.avoid_gfx_accel=true \
+    ro.vendor.qti.am.reschedule_service=true \
+    ro.vendor.qti.sys.fw.bservice_enable=true
